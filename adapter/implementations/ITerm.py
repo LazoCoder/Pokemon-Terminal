ddee836e1eda57793b4a6738b7f8314685ff97f0
Fiskie <myself@fiskie.me>
2017-06-21 13:46:47 +0100

    Pipe a script directly to osascript instead of doing weird file stuff

--- adapter/implementations/ITerm.py
+++ adapter/implementations/ITerm.py
@@ -1,16 +1,11 @@
 import os
+import subprocess
 
 from adapter.base import TerminalAdapterInterface
 
 
-# todo: broken probably
-# do we really need to use applescript? would be nice to
-# find a way to do this without.
-cwd = os.path.dirname(os.path.realpath(__file__))
-
-
 class ITerm(TerminalAdapterInterface):
-    def __terminal_script(self, path):
+    def __generate_osascript(self, path):
         # Create the content for script that will change the terminal background image.
         content = "tell application \"iTerm\"\n"
         content += "\ttell current session of current window\n"
@@ -19,36 +14,16 @@ class ITerm(TerminalAdapterInterface):
         content += "end tell"
         return content
 
-    def __iterm2_clear_script(self):
-        # Create and save the script for clearing the terminal background image.
-        content = self.__terminal_script("")
-        file = open(cwd + "/./Scripts/background.scpt", "wb")
-        file.write(bytes(content, 'UTF-8'))
-        file.close()
-
-    def __darwin_create_terminal_bash(self):
-        # Create and save the run.sh that will execute the AppleScript if the correct run.sh doesn't already exist.
-        content = "#!/bin/bash\n" + "osascript " + cwd + "/./Scripts/background.scpt"
-        if open(cwd + "/./Scripts/run.sh", 'r').read() == content:
-            return
-        file = open(cwd + "/./Scripts/run.sh", 'wb')
-        file.write(bytes(content, 'UTF-8'))
-        file.close()
-
-    def __iterm2_create_terminal_script(self, pokemon):
-        # Create and save the script for changing the terminal background image.
-        content = self.__terminal_script(pokemon.get_path())
-        file = open(cwd + "/./Scripts/background.scpt", "wb")
-        file.write(bytes(content, 'UTF-8'))
-        file.close()
+    def __run_osascript(self, stream):
+        p = subprocess.Popen(['osascript'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        p.stdin.write(stream)
+        p.communicate()
+        p.stdin.close()
 
     def clear(self):
-        self.__iterm2_clear_script()
-        self.__darwin_create_terminal_bash()
-        os.system(cwd + "/./Scripts/run.sh")
+        stdin = self.__generate_osascript("")
+        self.__run_osascript(str.encode(stdin))
 
     def set_pokemon(self, pokemon):
-        # Create, save and run the bash script to change the terminal background.
-        self.__iterm2_create_terminal_script(pokemon)
-        self.__darwin_create_terminal_bash()
-        os.system(cwd + "/./Scripts/run.sh")
+        stdin = self.__generate_osascript(pokemon.get_path())
+        self.__run_osascript(str.encode(stdin))
