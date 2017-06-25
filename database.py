62fceca4fae9c26d0a0301a97f6f3efd76eb22f9
Griffin Young <griffinyoung50@gmail.com>
2017-06-25 12:25:40 -0700

    changed id -> name in light/dark data file

--- database.py
+++ database.py
@@ -97,19 +97,17 @@ class Database:
         with open(self.directory + "/./Data/light-dark.txt", 'r') as data_file:
             lines = [line.strip() for line in data_file.readlines()]
         values = [float(line.split(' ')[1]) for line in lines]
-        ids = [int(line.split(' ')[0]) for line in lines]
-        light = [ids[i] for i,v in enumerate(values) if v > threshold]
-        names = [self.__pokemon_list[i-1] for i in light]
-        return names if all else random.choice(names)
+        names = [line.split(' ')[0] for line in lines]
+        light = [names[i] for i,v in enumerate(values) if v > threshold]
+        return light if all else random.choice(light)
 
     def get_dark(self, threshold=0.6, all=False):
         with open(self.directory + "/./Data/light-dark.txt", 'r') as data_file:
             lines = [line.strip() for line in data_file.readlines()]
         values = [float(line.split(' ')[1]) for line in lines]
-        ids = [int(line.split(' ')[0]) for line in lines]
-        dark = [ids[i] for i,v in enumerate(values) if v < threshold]
-        names = [self.__pokemon_list[i-1] for i in dark]
-        return names if all else random.choice(names)
+        names = [line.split(' ')[0] for line in lines]
+        dark = [names[i] for i,v in enumerate(values) if v < threshold]
+        return dark if all else random.choice(dark)
 
     def __get_region(self, region):
         # Helper method for getting all the Pokemon of a specified region.
