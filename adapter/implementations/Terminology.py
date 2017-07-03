7e07dd10bfec8b9a4433356f72fbcf98eaeb65a1
cclauss <cclauss@bluewin.ch>
2017-07-03 16:44:34 +0200

    Terminology.py accepts an image_file_path, not a pokemon
    See issue #83

--- adapter/implementations/Terminology.py
+++ adapter/implementations/Terminology.py
@@ -8,7 +8,7 @@ class Terminology(TerminalAdapterInterface):
     def is_available():
         return os.environ.get("TERMINOLOGY") == '1'
 
-    def set_pokemon(self, image_file_path):
+    def set_image_file_path(self, image_file_path):
         os.system('tybg "{}"'.format(image_file_path))
 
     def clear(self):
