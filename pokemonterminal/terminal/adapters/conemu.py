bed790df557f6a2c4fa1ee312d75d9592884b9a0
Jimmy O'Rourke <jimmy.orourke@thalmic.com>
2018-03-25 21:06:21 -0400

    fix conemu commands and image paths

--- pokemonterminal/terminal/adapters/conemu.py
+++ pokemonterminal/terminal/adapters/conemu.py
@@ -11,10 +11,16 @@ class ConEmuProvider(_TProv):
         return "CONEMUPID" in os.environ
 
     def change_terminal(path: str):
-        subprocess.run(['ConEmuC', '-GuiMacro', 'SetOption("Background Image", "{}")'.format(path)], check=True)
+        # ConEmu has issues with the quoting when using an arg list, so use shell=True
+        # ConEmu requires the image path to have escaped slashes. Enforce this by converting all slashes in the path to
+        # backlashes, replace all previously doubled slashes with a single slash, then replace all single slashes with
+        # double backslash
+        cmd = 'ConEmuC -GuiMacro SetOption("Background Image", "{}")'.format(os.path.normpath(path).replace(
+            '\\\\', '\\').replace('\\', '\\\\'))
+        subprocess.run(cmd, shell=True, check=True)
 
     def clear():
-        subprocess.run(['ConEmuC', '-GuiMacro', 'SetOption("Background Image", "")'], check=True)
+        subprocess.run('ConEmuC -GuiMacro SetOption("Background Image", "")', shell=True, check=True)
 
     def __str__():
         return "ConEmu"
