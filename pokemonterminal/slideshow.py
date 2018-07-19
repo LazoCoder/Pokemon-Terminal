875a41cb8d6356ef219216651b7f3e12887b2258
Charles Milette <me@charlesmilette.net>
2018-07-18 23:04:57 -0400

    Add win32 slideshow

--- pokemonterminal/slideshow.py
+++ pokemonterminal/slideshow.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.6
-import os
+import atexit
 import random
 import sys
-import time
 
 from threading import Thread
 
@@ -28,9 +27,16 @@ def __slideshow_worker(filtered, delay, changer_func):
         t.join(delay * 60)
 
 def start(filtered, delay, changer_func):
-    if os.name == 'nt':
-        raise Exception('TODO')
+    if sys.platform == 'win32':
+        import multiprocessing
+        p = multiprocessing.Process(target=__slideshow_worker, args=(filtered, delay, changer_func, ), daemon=True)
+        p.start()
+        __print_fork(p.pid, len(filtered), delay)
+        # HACK remove multiprocessing's exit handler to prevent it killing our child.
+        atexit.unregister(multiprocessing.util._exit_function)
+        sys.exit(0)
     else:
+        import os
         pid = os.fork()
     if pid > 0:
         __print_fork(pid, len(filtered), delay)
