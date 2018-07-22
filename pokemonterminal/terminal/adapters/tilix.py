7dab5737ce9cb1996c5c2512af580e76f02419ae
Charles Milette <me@charlesmilette.net>
2018-07-22 19:32:38 -0400

    Consistency fixes

--- pokemonterminal/terminal/adapters/tilix.py
+++ pokemonterminal/terminal/adapters/tilix.py
@@ -5,18 +5,18 @@ from . import TerminalProvider as _TProv
 
 
 class TilixProvider(_TProv):
-    setting_key = "com.gexperts.Tilix.Settings"
-    setting_field = "background-image"
+    __setting_key = "com.gexperts.Tilix.Settings"
+    __setting_field = "background-image"
 
     def is_compatible() -> bool:
         return "TILIX_ID" in environ
 
     def change_terminal(path: str):
-        command = f'gsettings set {TilixProvider.setting_key} {TilixProvider.setting_field} "{path}"'
+        command = f'gsettings set {TilixProvider.setting_key} {TilixProvider.__setting_field} "{path}"'
         run(command, check=True)
 
     def clear():
-        command = f'gsettings reset {TilixProvider.setting_key} {TilixProvider.setting_field}'
+        command = f'gsettings reset {TilixProvider.setting_key} {TilixProvider.__setting_field}'
         run(command, check=True)
 
     def __str__():
