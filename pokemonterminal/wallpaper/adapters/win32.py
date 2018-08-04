54c18fa7bd424767b39235dee063215c24848d43
Samuel Henrique Oliveira da Silva <samosaara@gmail.com>
2018-08-03 23:42:32 -0300

    Fixes wrong class name reference.

--- pokemonterminal/wallpaper/adapters/win32.py
+++ pokemonterminal/wallpaper/adapters/win32.py
@@ -7,7 +7,7 @@ class Win32Provider(_WProv):
     __SPI_SETDESKWALLPAPER = 20
 
     def change_wallpaper(path: str):
-        windll.user32.SystemParametersInfoW(WindowsProvider.__SPI_SETDESKWALLPAPER, 0, path, 0)
+        windll.user32.SystemParametersInfoW(Win32Provider.__SPI_SETDESKWALLPAPER, 0, path, 0)
 
     def is_compatible() -> bool:
         return sys.platform == "win32"
