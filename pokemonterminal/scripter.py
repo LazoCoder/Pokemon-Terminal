271b12c387cf6b81f9aa095e41ce05f57e980a88
Charles Milette <me@charlesmilette.net>
2018-08-12 16:15:49 -0400

    Fix bad english

--- pokemonterminal/scripter.py
+++ pokemonterminal/scripter.py
@@ -16,7 +16,7 @@ def __init_terminal_provider():
     if len(providers) > 1:
         # All this if is really not supposed to happen at all whatsoever
         # really what kind of person has 2 simultaneous T.E???
-        print("Multiple providers found select the appropriate one:")
+        print("Multiple providers found, please select the appropriate one.")
         for i, x in enumerate(providers):
             print(f'{i}. {x.__str__()}')
         print("If some of these make no sense or are irrelevant please file " +
@@ -46,7 +46,7 @@ def __init_wallpaper_provider():
     if len(providers) > 1:
         # All this if is really not supposed to happen at all whatsoever
         # really what kind of person has 2 simultaneous D.E???
-        print("Multiple providers found select the appropriate one:")
+        print("Multiple providers found, please select the appropriate one.")
         for i, x in enumerate(providers):
             print(f'{i}. {x.__str__()}')
         print("If some of these make no sense or are irrelevant please file " +
