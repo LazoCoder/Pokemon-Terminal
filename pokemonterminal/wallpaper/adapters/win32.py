5a2353a17c76bfa9ebfc098a7af2981320b05785
cclauss <cclauss@me.com>
2019-03-28 05:18:37 +0100

    ctypes.windll...
    https://stackoverflow.com/questions/34040123/ctypes-cannot-import-windll

--- pokemonterminal/wallpaper/adapters/win32.py
+++ pokemonterminal/wallpaper/adapters/win32.py
@@ -1,14 +1,14 @@
 import sys
 
+from ctypes
 from . import WallpaperProvider as _WProv
-from ctypes import windll
 
 
 class Win32Provider(_WProv):
     __SPI_SETDESKWALLPAPER = 20
 
     def change_wallpaper(path: str):
-        windll.user32.SystemParametersInfoW(Win32Provider.__SPI_SETDESKWALLPAPER, 0, path, 0)
+        ctypes.windll.user32.SystemParametersInfoW(Win32Provider.__SPI_SETDESKWALLPAPER, 0, path, 0)
 
     def is_compatible() -> bool:
         return sys.platform == "win32"
