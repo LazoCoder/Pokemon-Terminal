b975da0db616a280e1cd7003ea253997eaccb2ed
Charles Milette <me@charlesmilette.net>
2019-06-13 11:24:32 -0400

    Automatically enable backgrounds in ConEmu

--- pokemonterminal/terminal/adapters/conemu.py
+++ pokemonterminal/terminal/adapters/conemu.py
@@ -15,12 +15,19 @@ class ConEmuProvider(_TProv):
         if output != 'OK':
             print(output)
 
+    def __enable_background(state: bool):
+        # https://github.com/Maximus5/ConEmu/blob/be9695f5b565c28379237a5fdea3abab7fea3e3a/src/ConEmu/resource.h#L282
+        ConEmuProvider.__run_command(f'SetOption("Check", 1699, {int(state)})')
+
     def change_terminal(path: str):
         # ConEmuC supports its own character escaping, so escape the backslashes just to be sure
         ConEmuProvider.__run_command('SetOption("Background Image", "{}")'.format(path.replace("\\", "\\\\")))
 
+        # Done after setting background image to avoid the old background flashing if it was disabled.
+        ConEmuProvider.__enable_background(True)
+
     def clear():
-        ConEmuProvider.__run_command('SetOption("Background Image", "")')
+        ConEmuProvider.__enable_background(False)
 
     def __str__():
         return "ConEmu"
