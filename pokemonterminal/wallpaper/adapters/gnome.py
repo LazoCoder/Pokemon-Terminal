c4191a0fed41a05b826c39ded6d320eeb4544703
Charles Milette <charles.milette@gmail.com>
2020-05-01 18:11:13 -0400

    Fix missing name

--- pokemonterminal/wallpaper/adapters/gnome.py
+++ pokemonterminal/wallpaper/adapters/gnome.py
@@ -11,7 +11,7 @@ class GnomeProvider(_WProv):
         run(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"file://{path}"], check=True)
 
     def is_compatible() -> bool:
-        return environ.get("DESKTOP_SESSION", default='').lower() in __session_names
+        return environ.get("DESKTOP_SESSION", default='').lower() in GnomeProvider.__session_names
 
     def __str__():
         return "GNOME Shell Desktop"
