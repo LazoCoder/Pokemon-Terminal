15c6a8daef4a1bfc05cd6be203910a6b86a461c4
Hannah Brooke <hannah@mail.yttrium.io>
2022-11-30 15:27:37 -0600

    support more wezterm config file locations

--- pokemonterminal/terminal/adapters/wezterm.py
+++ pokemonterminal/terminal/adapters/wezterm.py
@@ -9,8 +9,21 @@ from . import TerminalProvider as _TProv
 
 class WezTermTerminalProvider(_TProv):
     @staticmethod
-    def __set_bg_image(new: Union[os.PathLike, str]):
-        with open(pathlib.Path(os.path.expanduser('~/.wezterm.lua')), 'r+') as cfg:
+    def __get_config_file():
+        # loosely follow the logic detailed at https://wezfurlong.org/wezterm/config/files.html#configuration-files
+
+        # can't check for the command line argument so start by looking at the
+        # environment variable
+        if (path := os.environ.get('WEZTERM_CONFIG_FILE', None)) is not None:
+            return pathlib.Path(path)
+        elif (path := os.environ.get('XDG_CONFIG_HOME', None)) is not None:
+            return pathlib.Path(path) / 'wezterm' / 'wezterm.lua'
+        else:
+            return pathlib.Path(os.path.expanduser('~/.wezterm.lua'))
+
+    @staticmethod
+    def __set_bg_image(new: Union[os.PathLike[str], str]):
+        with open(WezTermTerminalProvider.__get_config_file(), 'r+') as cfg:
             tree = ast.parse(cfg.read())
             dirty = False
             for node in ast.walk(tree):
@@ -28,20 +41,20 @@ class WezTermTerminalProvider(_TProv):
 
                     # if the node already exists then modify it
                     if len(list(filter(
-                        lambda f: f.key.id == 'window_background_image',
-                        node.values[0].fields))) > 0:
-                        
+                            lambda f: f.key.id == 'window_background_image',
+                            node.values[0].fields))) > 0:
+
                         for field in node.values[0].fields:
                             if field.key.id == 'window_background_image' and \
                                     field.value.s != new:
                                 field.value = astnodes.String(
-                                    new, field.value.delimiter
+                                    str(new), field.value.delimiter
                                 )
                                 dirty = True
                     else:  # otherwise we will append it to the nodes
                         node.values[0].fields.append(astnodes.Field(
                             astnodes.Name('window_background_image'),
-                            astnodes.String(new)
+                            astnodes.String(str(new))
                         ))
                         dirty = True
             # technically there's no code path here that doesn't result in dirty
@@ -67,4 +80,3 @@ class WezTermTerminalProvider(_TProv):
     @staticmethod
     def change_terminal(path: str):
         WezTermTerminalProvider.__set_bg_image(path)
-
