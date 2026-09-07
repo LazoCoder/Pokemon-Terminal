d978aca59be0266ddeae81fbd3dd34442ebfdbbe
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-09-07 22:35:43 +0100

    Finishing touches.
    Added a second indented try loop that uses that backup URL, followed by another except that allows a failed URL to be skipped, and the corresponding Pokémon's name to be displayed for easy troubleshooting.
    
    Wish I thought of that 3 months sooner...

--- pokemonterminal/Images/Pokemon_Info_Compiler.py
+++ pokemonterminal/Images/Pokemon_Info_Compiler.py
@@ -3,18 +3,23 @@ import Pokemon_Image_Brightness_Finder as imageBrightness
 import requests
 
 # folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
-folderPath = '/home/adam/Pokemon_Images/Adams_Tests/HQ_Images/'
+# folderPath = '/home/adam/Pokemon_Images/Adams_Tests/HQ_Images/'
+folderPath = '/Pokemon_Python/HQ_Images/'
 
 list = os.listdir(folderPath)
 list.sort()
 
 # open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info.txt', 'w').close()
 # open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info_Log.txt', 'w').close()
-open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info.txt', 'w').close()
-open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info_Log.txt', 'w').close()
+# open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info.txt', 'w').close()
+# open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info_Log.txt', 'w').close()
+open('/Pokemon_Python/Pokemon_Compiled_Info.txt', 'w').close()
+open('/Pokemon_Python/Pokemon_Compiled_Info_Log.txt', 'w').close()
+
 
 # pkmnNameLog = open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info_Log.txt').read().split()
-pkmnNameLog = open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt').read().split()
+# pkmnNameLog = open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt').read().split()
+pkmnNameLog = open('/Pokemon_Python/Pokemon_Compiled_Info_Log.txt').read().split()
 
 testCount = 0
 
@@ -22,7 +27,7 @@ for fileName in list:
     pokemonDexNo = fileName[:4]
     pokemonDexNo = int(pokemonDexNo)
     pokemonDexNoFixed = ("{:04d}".format(pokemonDexNo))
-    # pokemonName = "Keldeo-Ordinary"
+    # pokemonName = "Wishiwashi-Solo"
     pokemonName = fileName[5:-4]
     pokemonNameFixed = pokemonName.replace('_', '-').replace("'", "").replace('.', '-').replace('.png', '')
 
@@ -40,52 +45,55 @@ for fileName in list:
 
         for item in pkmnTypesVariable:
             pkmnTypeList = pkmnTypeList + '\t' + (item['type']['name']).title()
-            print(pkmnTypeList)
+            # print(pkmnTypeList)
 
         
         for item in pkmnAbilitiesVariable:
             if not item['is_hidden']:
                 pkmnAbilityList = pkmnAbilityList + '\t' + (item['ability']['name']).title()
-                print(pkmnAbilityList)
+                # print(pkmnAbilityList)
 
         for item in pkmnAbilitiesVariable:
             if item['is_hidden']:
                 pkmnHiddenAbilityList = (item['ability']['name']).title()
-                print(pkmnHiddenAbilityList)
+                # print(pkmnHiddenAbilityList)
 
         
 
     except:
-        pkmnSearchVariableBackup = requests.get("https://pokeapi.co/api/v2/pokemon-form/" + pokemonNameFixed)
-        pkmnURLVariable = pkmnSearchVariableBackup.json()['pokemon']['url']
-        pkmnSearchVariable = requests.get(pkmnURLVariable)
+        try:
+            pkmnSearchVariableBackup = requests.get("https://pokeapi.co/api/v2/pokemon-form/" + pokemonNameFixed)
+            pkmnURLVariable = pkmnSearchVariableBackup.json()['pokemon']['url']
+            pkmnSearchVariable = requests.get(pkmnURLVariable)
 
-        pkmnNameVariable = pkmnSearchVariable.json()['name'].title()
-        pkmnTypesVariable = pkmnSearchVariable.json()['types']
-        pkmnAbilitiesVariable = pkmnSearchVariable.json()['abilities']
+            pkmnNameVariable = pkmnSearchVariable.json()['name'].title()
+            pkmnTypesVariable = pkmnSearchVariable.json()['types']
+            pkmnAbilitiesVariable = pkmnSearchVariable.json()['abilities']
 
-        for item in pkmnTypesVariable:
-            pkmnTypeList = pkmnTypeList + '\t' + (item['type']['name']).title()
-            print(pkmnTypeList)
-        
-                
-        for item in pkmnAbilitiesVariable:
-            if not item['is_hidden']:
-                pkmnAbilityList = pkmnAbilityList + '\t' + (item['ability']['name']).title()
-                print(pkmnAbilityList)
-        
-        for item in pkmnAbilitiesVariable:
-            if item['is_hidden']:
-                pkmnHiddenAbilityList = (item['ability']['name']).title()
-                print(pkmnHiddenAbilityList)
+            for item in pkmnTypesVariable:
+                pkmnTypeList = pkmnTypeList + '\t' + (item['type']['name']).title()
+                # print(pkmnTypeList)
+            
+                    
+            for item in pkmnAbilitiesVariable:
+                if not item['is_hidden']:
+                    pkmnAbilityList = pkmnAbilityList + '\t' + (item['ability']['name']).title()
+                    # print(pkmnAbilityList)
+            
+            for item in pkmnAbilitiesVariable:
+                if item['is_hidden']:
+                    pkmnHiddenAbilityList = (item['ability']['name']).title()
+                    # print(pkmnHiddenAbilityList)
 
-        print("Failsafe Used")
+        except:
+            print(pokemonName)
 
 
     # with open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info.txt', 'a') as compiledCsv:
-    with open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info.txt', 'a') as compiledCsv:
+    # with open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info.txt', 'a') as compiledCsv:
+    with open('/Pokemon_Python/Pokemon_Compiled_Info.txt', 'a') as compiledCsv:
         compiledCsv.write(str(pokemonDexNoFixed) + '\t' + pokemonName.title() + '\t' + str(imageBrightness.pokemonImageBrightnessFinder(folderPath, fileName)) + '\t' + pkmnTypeList + '\t' + pkmnAbilityList + '\t' + pkmnHiddenAbilityList + '\n')
     # with open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info_Log.txt', 'a') as infoLog:
-    with open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info_Log.txt', 'a') as infoLog:
+    # with open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Compiled_Info_Log.txt', 'a') as infoLog:
+    with open('/Pokemon_Python/Pokemon_Compiled_Info_Log.txt', 'a') as infoLog:
         infoLog.write(fileName + '\n')
-    
