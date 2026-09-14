345e847bf4cadbef001713aaf0a17013600314b6
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-09-14 12:11:04 +0100

    Testing some things

--- pokemonterminal/Images/Pokemon_Common_Colour_Finder.py
+++ pokemonterminal/Images/Pokemon_Common_Colour_Finder.py
@@ -1,7 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageChops
 
-def mostCommonUsedColour(image):
-    imageWidth, imageHeight = image.size
+width = 1366
+height = 768
+
+def mostCommonUsedColour(imageInv):
+    imageWidth, imageHeight = imageInv.size
 
     redTotal = 0
     greenTotal = 0
@@ -12,7 +15,7 @@ def mostCommonUsedColour(image):
 
     for x in range(0, imageWidth):
         for y in range(0, imageHeight):
-            red, green, blue, alpha = image.getpixel((x, y))
+            red, green, blue, alpha = imageInv.getpixel((x, y))
 
             if alpha == 0:
                 continue
@@ -26,9 +29,13 @@ def mostCommonUsedColour(image):
 
     return (redTotal/count, greenTotal/count, blueTotal/count, alphaTotal/count)
 
-image = Image.open(r'/home/adam/Pokemon_Images/Adams_Tests/HQ_Spare_Images/0655_Delphox.png')
+image = Image.open('/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/0655_Delphox-Mega.png')
+# image = Image.open('/home/mark/Adams_Dev_Test/test.png')
+# image = Image.open(r'/home/adam/Pokemon_Images/Adams_Tests/HQ_Spare_Images/0655_Delphox.png')
 
 image = image.convert('RGBA')
+
+# imageInv = ImageChops.invert(image)
 
 commonColour = mostCommonUsedColour(image)
 
@@ -41,3 +48,6 @@ print(hex(int(redCommon)))
 print(hex(int(greenCommon)))
 print(hex(int(blueCommon)))
 print(hex(int(alphaCommon)))
+
+img = Image.new(mode = "RGB", size = (width, height), color = (int(redCommon), int(greenCommon), int(blueCommon)))
+img.show()
